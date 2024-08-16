1d91945c222373db944e554fbe5443fe44272cd2
AhmedGaberElbltagy <ahmedelbltagy1999@gmail.com>
2024-08-16 18:27:19 +0300

    start developing on the java application

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "index.html";
+    }
+
+}
